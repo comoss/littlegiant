f13972506e4c77d5e98daf33907a8d645d5babe8
Cody Moss <codymoss@Vickies-MacBook-Pro.local>
2015-05-07 23:35:20 -0600

    reduced the size of assets and oragnized the images according to page

--- scripts/controllers/defenderCarouselController.js
+++ scripts/controllers/defenderCarouselController.js
@@ -2,19 +2,19 @@ angular.module('lgApp').controller('defenderCarousel', function ($scope) {
   $scope.myInterval = 4000;
   var slides = $scope.slides = [
        {image: 'images/LOGO, LGLS, CLIMB ON-page-001.jpg', text: 'DEFENDER'},
-       {image: 'images/Defender17 Ratchet copy.jpg', text: ''},
-       {image: 'images/Defender17 Carry Ex.jpg', text: ''},
-       {image: 'images/Defender17 Carry Stairs.jpg', text: ''},
-       {image: 'images/Defender17 AFrame.jpg', text: ''},
-       {image: 'images/Defender17 Carry1 copy.JPG', text: ''},
-       {image: 'images/Defender17 EX copy.jpg', text: ''},
-       {image: 'images/Defender17 EX4 copy.jpg', text: ''},
-       {image: 'images/Defender17 Wheel2.jpg', text: ''},
-       {image: 'images/Defender17 Ratchet Close copy.jpg', text: ''},
-       {image: 'images/Defender17 EX2 copy.jpg', text: ''},
-       {image: 'iimages/GlowLabel.jpg', text: ''},
-       {image: 'images/dsc_0088.jpg', text: ''},
-       {image: 'images/https://lh3.googleusercontent.com/-UtsBJshcTgI/VT70vRO1x7I/AAAAAAAAAEE/V2i-sPxEJQQ/w689-h891-no/defender', text: ''},
+       {image: 'images/defender/Defender17 Ratchet copy.jpg', text: ''},
+       {image: 'images/defender/Defender17 Carry Ex.jpg', text: ''},
+       {image: 'images/defender/Defender17 Carry Stairs.jpg', text: ''},
+       {image: 'images/defender/Defender17 AFrame.jpg', text: ''},
+       {image: 'images/defender/Defender17 Carry1 copy.JPG', text: ''},
+       {image: 'images/defender/Defender17 EX copy.jpg', text: ''},
+       {image: 'images/defender/Defender17 EX4 copy.jpg', text: ''},
+       {image: 'images/defender/Defender17 Wheel2.jpg', text: ''},
+       {image: 'images/defender/Defender17 Ratchet Close copy.jpg', text: ''},
+       {image: 'images/defender/Defender17 EX2 copy.jpg', text: ''},
+       {image: 'images/defender/Defender17 90 1.jpg', text: ''},
+       {image: 'images/defender/Defender17 Carry5 copy.jpg', text: ''},
+       {image: 'https://lh3.googleusercontent.com/-UtsBJshcTgI/VT70vRO1x7I/AAAAAAAAAEE/V2i-sPxEJQQ/w689-h891-no/defender', text: ''},
   ];
 });			 
 
